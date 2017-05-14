d442d96497aeb12f77847411d6c3531d8c1900d4
vildozo <vildozo@outlook.com>
2017-05-14 16:01:24 -0400

    codigo original

---
+++ src/test/java/it/xpug/birthday_greetings/EmployeeTest.java
@@ -0,0 +1,31 @@
+package it.xpug.birthday_greetings;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import it.xpug.birthday_greetings.Employee;
+import it.xpug.birthday_greetings.XDate;
+
+
+
+public class EmployeeTest {
+
+	@Test
+	public void testBirthday() throws Exception {
+		Employee employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
+		assertFalse("not his birthday", employee.isBirthday(new XDate("2008/01/30")));
+		assertTrue("his birthday", employee.isBirthday(new XDate("2008/01/31")));
+	}
+
+	@Test
+	public void equality() throws Exception {
+		Employee base = new Employee("First", "Last", "1999/09/01", "first@last.com");
+		Employee same = new Employee("First", "Last", "1999/09/01", "first@last.com");
+		Employee different = new Employee("First", "Last", "1999/09/01", "boom@boom.com");
+
+		assertFalse(base.equals(null));
+		assertFalse(base.equals(""));
+		assertTrue(base.equals(same));
+		assertFalse(base.equals(different));
+	}
+}
